97424d488c504ef5f542149e42130bab7d9090fa
JSungMin <jsm1505104@gmail.com>
2016-01-03 00:57:22 +0900

    BaseAsset
    에셋 폴더 커밋

---
+++ Assets/ProCamera2D/Examples/Platformer/Scripts/Player/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Com.LuisPedroFonseca.ProCamera2D.Platformer
+{
+	[RequireComponent(typeof(SphereCollider))]
+	[RequireComponent(typeof(Rigidbody))]
+	public class PlayerController : MonoBehaviour
+	{
+	    public float PlayerSpeed = 5.5f;
+
+	    public MovementAxis Axis;
+
+	    Vector3 _targetVelocity = Vector3.right;
+
+	    void FixedUpdate()
+	    {
+	    	
+	        
+	        _targetVelocity *= PlayerSpeed;
+	        GetComponent<Rigidbody>().AddForce(_targetVelocity, ForceMode.Force);
+	    }
+	}
+}
---
+++ Assets/Scripts/CharacterMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMove : MonoBehaviour {
+	public float playerSpeed;
+	public float gravity = 20;
+	public float accelation = 30;
+	public float jumpHeight = 12;
+	// Use this for initialization
+	void Start () {
+		playerSpeed = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if()
+		{
+
+		}
+	}
+}
